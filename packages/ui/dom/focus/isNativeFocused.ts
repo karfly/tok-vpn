457c8144bb0493e3c2f3c95e41ae4d42f9895762
Beskrovnyi.KE <Beskrovnyi.KE@medsigroup.ru>
2023-10-02 01:42:01 +0300

    feat: cleanup

--- packages/ui/dom/focus/isNativeFocused.ts
+++
@@ -1,9 +0,0 @@
-import { getNativeFocused } from './getNativeFocused';
-
-export function isNativeFocused(node: Node | null): boolean {
-  return (
-    !!node &&
-    !!node.ownerDocument &&
-    getNativeFocused(node.ownerDocument) === node
-  );
-}
