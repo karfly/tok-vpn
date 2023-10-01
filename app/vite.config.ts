c896e2a25f0b945ede58e0a954607e1906a1d504
Beskrovnyi.KE <Beskrovnyi.KE@medsigroup.ru>
2023-10-01 22:35:29 +0300

    feat: fix base href

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,38 +1,42 @@
 import vue from '@vitejs/plugin-vue';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import svgLoader from 'vite-svg-loader';
 
 import { telegramStickerLoader } from './_internal/tgs.loader';
 
-export default defineConfig({
-  base: '/tok',
-  plugins: [
-    telegramStickerLoader(),
-    vue(),
-    svgLoader({
-      defaultImport: 'component',
-      svgoConfig: {
-        plugins: [
-          {
-            name: 'cleanupIds',
-            params: {
-              remove: false,
-              minify: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.base || '/',
+    plugins: [
+      telegramStickerLoader(),
+      vue(),
+      svgLoader({
+        defaultImport: 'component',
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'cleanupIds',
+              params: {
+                remove: false,
+                minify: false,
+              },
             },
-          },
-        ],
-      },
-    }),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "@tok/ui/styles/local.scss";',
+          ],
+        },
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@tok/ui/styles/local.scss";',
+        },
       },
     },
-  },
-  build: {
-    assetsInlineLimit: 0,
-    minify: true,
-  },
+    build: {
+      assetsInlineLimit: 0,
+      minify: true,
+    },
+  };
 });
