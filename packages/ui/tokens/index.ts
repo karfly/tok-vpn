cc06a4c496e63540e9fbe2117e954e175fe171bc
Beskrovnyi.KE <Beskrovnyi.KE@medsigroup.ru>
2023-10-05 18:57:12 +0300

    feat: fix name

--- packages/ui/tokens/index.ts
+++ packages/ui/tokens/index.ts
@@ -1,4 +1,4 @@
-export * from './_currencyOptions.token';
 export * from './AlertsHost.token';
+export * from './CurrencyOptions.token';
 export * from './CustomIcons.token';
 export * from './PopupsHost.token';
